6e8b146a0e01dc0bfcecea84f82051542ad353a4
r3bbitiscute <chongkhaixuen0314@gmail.com>
2025-02-16 16:41:01 +0800

    Updated commenting, variable name and function name

--- Assets/Scripts/Movement/SphereCollider.cs
+++ Assets/Scripts/Movement/SphereCollider.cs
@@ -31,7 +31,7 @@ public class SphereCollider : MonoBehaviour
     }
 
     /// <summary>
-    /// Find all Game Object with the tags "Wall" and "Floor" and assign their bounds into collideObjects for further calculation
+    /// Find all GameObjects with the tags "Wall" and "Ground" and assign their bounds into collideObjects for further calculation
     /// </summary>
     private void FindObstacles()
     {
@@ -59,7 +59,7 @@ public class SphereCollider : MonoBehaviour
         // Looping through each object and see which points on the bounds are closest to player
         foreach (var obj in collideObjects)
         {
-            Vector3 closestPoint = GetClosestPointOnBounds(transform.position, obj);
+            Vector3 closestPoint = GetNearestPointOnBounds(transform.position, obj);
             float distance = Vector3.Distance(transform.position, closestPoint);
 
             // If inside obstacle, push sphere out
@@ -73,14 +73,14 @@ public class SphereCollider : MonoBehaviour
     }
 
     /// <summary>
-    /// Get the closest point on a box (wall/floor) to a sphere
-    /// </summary>
-    private Vector3 GetClosestPointOnBounds(Vector3 centerPoint, Bounds bounds)
+    /// Finds the closest point of the object's bounds according to the player position.
+    /// <summary>
+    private Vector3 GetNearestPointOnBounds(Vector3 playerPos, Bounds bounds)
     {
         // Note: Bounds.min is the bottom left corner of the object, while Bounds.max is the top right corner of the object
-        float clampedX = Mathf.Clamp(centerPoint.x, bounds.min.x, bounds.max.x);
-        float clampedY = Mathf.Clamp(centerPoint.y, bounds.min.y, bounds.max.y);
-        float clampedZ = Mathf.Clamp(centerPoint.z, bounds.min.z, bounds.max.z);
+        float clampedX = Mathf.Clamp(playerPos.x, bounds.min.x, bounds.max.x);
+        float clampedY = Mathf.Clamp(playerPos.y, bounds.min.y, bounds.max.y);
+        float clampedZ = Mathf.Clamp(playerPos.z, bounds.min.z, bounds.max.z);
         return new Vector3(clampedX, clampedY, clampedZ);
     }
 }
